d0e16abe27943e898eb81bb77d727bf33c9216f6
David <david.owuor@student.moringaschool.com>
2022-11-22 15:32:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet){
+  if(feet>2500){
+    return "No can do."
+  }if(feet <= 400){
+    return 'This one is on me!'
+} else if(feet=2001){
+  return 'I will gladly take your thirty bucks.'
+}
+}
+  
+
+
+
+
+
+
+
+
+function ternaryCheckCity(city){
+  //  if(city==="NYC"){
+  //  return "Ok, sounds good."
+  //  }else {
+  //   return "No go."
+  //  }
+  return city==="NYC"? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous" : 
+    return 'Thank you so much.'
+    case 'not as generous' :
+    return 'Thank you.'
+    default :
+    return "Bye."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
